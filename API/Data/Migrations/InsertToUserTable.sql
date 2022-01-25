66f930256e177588a5a9ee081025f2f3ee653b81
Tresor Tshimanga <tshims79@gmail.com>
2022-01-25 21:59:56 +0200

    End of section 2

---
+++ API/Data/Migrations/InsertToUserTable.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Bob");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Ida");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Grace");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Eben");
